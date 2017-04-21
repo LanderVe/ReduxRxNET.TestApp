a7d282a014ac9a89a1b1d757c4368ddb3a322991
Lander Verhack <landerve@users.noreply.github.com>
2017-04-21 17:30:04 +0200

    moved testapp to its own project

---
+++ TestApp/State/Actions/DataActions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Models;
+
+namespace TestApp.State.Actions
+{
+  internal class LoadContactsAction { }
+  internal class LoadContactsSuccessAction
+  {
+    private readonly ImmutableList<Contact> data;
+
+    public LoadContactsSuccessAction(ImmutableList<Contact> data)
+    {
+      this.data = data;
+    }
+    public ImmutableList<Contact> Data => data;
+  }
+  internal class LoadContactsFailAction { }
+
+
+  internal class SaveNewContactAction {
+    private readonly Contact contact;
+
+    public SaveNewContactAction(Contact contact)
+    {
+      this.contact = contact;
+    }
+    public Contact Contact => contact;
+  }
+  internal class SaveNewContactSuccessAction
+  {
+    private readonly Contact contact;
+
+    public SaveNewContactSuccessAction(Contact contact)
+    {
+      this.contact = contact;
+    }
+    public Contact Contact => contact;
+  }
+  internal class SaveNewContactFailAction { }
+
+
+  internal class UpdateContactAction {
+    private readonly Contact contact;
+
+    public UpdateContactAction(Contact contact)
+    {
+      this.contact = contact;
+    }
+    public Contact Contact => contact;
+  }
+  internal class UpdateContactSuccessAction
+  {
+    private readonly Contact contact;
+
+    public UpdateContactSuccessAction(Contact contact)
+    {
+      this.contact = contact;
+    }
+    public Contact Contact => contact;
+  }
+  internal class UpdateContactFailAction { }
+
+
+  internal class DeleteContactAction {
+    private readonly int contactId;
+
+    public DeleteContactAction(int contactId)
+    {
+      this.contactId = contactId;
+    }
+    public int ContactId => contactId;
+  }
+  internal class DeleteContactSuccessAction
+  {
+    private readonly int contactId;
+
+    public DeleteContactSuccessAction(int contactId)
+    {
+      this.contactId = contactId;
+    }
+    public int ContactId => contactId;
+  }
+  internal class DeleteContactFailAction { }
+
+
+}
---
+++ TestApp/State/Shape/ListState.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.State.Shape
+{
+  public class ListState
+  {
+    private readonly ListSearchState search;
+    private readonly ListSelectedItemState selectedItem;
+
+    public ListState(ListSearchState search, ListSelectedItemState selectedItem)
+    {
+      this.search = search;
+      this.selectedItem = selectedItem;
+    }
+
+    public ListSearchState Search => search;
+    public ListSelectedItemState SelectedItem => selectedItem;
+  }
+
+  public class ListSearchState
+  {
+    private readonly string searchTerm;
+    private readonly bool isSearching;
+    private readonly ImmutableList<int> contactIds;
+
+    public ListSearchState(string searchTerm, bool isSearching, ImmutableList<int> contactIds)
+    {
+      this.searchTerm = searchTerm;
+      this.isSearching = isSearching;
+      this.contactIds = contactIds;
+    }
+
+    public string SearchTerm => searchTerm;
+    public bool IsSearching => isSearching;
+    public ImmutableList<int> ContactIds => contactIds;
+  }
+
+  public class ListSelectedItemState
+  {
+    private readonly int selectedId;
+    private readonly bool isSelectedSaving; // update, delete, insert
+    private readonly bool isSelectedNew; // when adding a new element, instead of editing an old one
+    private readonly string error;
+
+    public ListSelectedItemState(int selectedId, bool isSelectedSaving, bool isSelectedNew, string error = null)
+    {
+      this.selectedId = selectedId;
+      this.isSelectedSaving = isSelectedSaving;
+      this.isSelectedNew = isSelectedNew;
+      this.error = error;
+    }
+
+    public int SelectedId => selectedId;
+    public bool IsSelectedSaving => isSelectedSaving;
+    public bool IsSelectedNew => isSelectedNew;
+    public string Error => error;
+  }
+}
